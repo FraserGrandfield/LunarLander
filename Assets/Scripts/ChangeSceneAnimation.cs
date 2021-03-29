348b46daebc9e443d2af31f14e4a8a76bd91a027
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2021-03-29 14:09:50 +0100

    Fade in fade out animations in main menu

---
+++ Assets/Scripts/ChangeSceneAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneAnimation : MonoBehaviour
+{
+    private Animator animator;
+    
+    private void OnEnable()
+    {
+        MainMenuButton.OnUIButtonClick += StartChangeSceneAnimation;
+    }
+
+    void Start()
+    {
+        animator = transform.gameObject.GetComponent<Animator>();
+    }
+
+    private void StartChangeSceneAnimation(string temp)
+    {
+        animator.SetBool("animateOut", true); 
+    }
+}
