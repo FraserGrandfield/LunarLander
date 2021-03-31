23e6a11a7e269a5a91931f2e1d4945235f0c9db2
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2021-03-31 20:55:43 +0100

    notificaitons prt2

---
+++ Assets/Scripts/UI/Notifications/Notification.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Notification : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayerListManager.ShowNotificaiton += ShowNotification;
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        PlayerListManager.ShowNotificaiton -= ShowNotification;
+    }
+
+    private void ShowNotification(string notificationText)
+    {
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().text = notificationText;
+        gameObject.SetActive(true);
+        StartCoroutine(HideNotification());
+    }
+
+    private IEnumerator HideNotification()
+    {
+        yield return new WaitForSeconds(3); 
+        gameObject.SetActive(false);
+    }
+}
